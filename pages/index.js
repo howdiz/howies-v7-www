3d2f0e5c0566d5d9ff2c0c0dd87de03a2570dbb9
Howie Ross <howard.ross@gmail.com>
2024-08-01 14:32:45 -0400

    move links into index

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import {mainContent, links, socialLinks} from './links.js';
 import { SocialIcon } from 'react-social-icons';
 
 const inter = Inter({ subsets: ["latin"] });
+
+const mainContent = {
+  imgSrc: `profile.png`,
+  name: `Howie Ross`,
+  bio1: `I’ve been building products on the internet for 20 years and leading teams for 10 years.`,
+  bio2: `I have worked for startups, agencies, and financial services companies, building websites, apps, e-commerce systems, and, cloud / edge platforms.`,
+  bio3: `I've also lectured for the UX/IxD Masters program at Thomas Jefferson University and spoke at conferences, meetups, and boot camps.`,
+  bio4: `I used to have a lot of hobbies like brewing beer, organize meetups including CTO School Philly, and be on charitable organization boards like the Philly Awesome Foundation. Now, I'm happily the father of twin boys, and I'm very into coffee.`
+};
 
 export default function Home() {
   return (
--- pages/links.js
+++
@@ -1,8 +0,0 @@
-export const mainContent = {
-  imgSrc: `profile.png`,
-  name: `Howie Ross`,
-  bio1: `I’ve been building products on the internet for 20 years and leading teams for 10 years.`,
-  bio2: `I have worked for startups, agencies, and financial services companies, building websites, apps, e-commerce systems, and, cloud / edge platforms.`,
-  bio3: `I've also lectured for the UX/IxD Masters program at Thomas Jefferson University and spoke at conferences, meetups, and boot camps.`,
-  bio4: `I used to have a lot of hobbies like brewing beer, organize meetups including CTO School Philly, and be on charitable organization boards like the Philly Awesome Foundation. Now, I'm happily the father of twin boys, and I'm very into coffee.`
-};
